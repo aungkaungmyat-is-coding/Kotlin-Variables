673b2dc8934e9bcc11e34bf971007cd3d512334f
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-02-27 21:09:14 +0630

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var userName: String = "Aung Kaung Myat"
+    var age: Int = 19
+    println("My name is $userName and I am $age years old.")
+}
